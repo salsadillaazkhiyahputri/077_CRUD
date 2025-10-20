1d8af3a79e3dffe6d42a78b1ca0bb89af3171ece
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-20 09:47:21 +0700

    tambah route dasar dan express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req,res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is runing on http://localhost:${PORT}`);
+})
+
