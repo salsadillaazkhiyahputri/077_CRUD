a31615aaa43f419d16e0f0a8e7eab963817cda26
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-10-20 10:21:16 +0700

    Menambahkan  CRUD

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(PORT, () => {
     console.log(`Server is runing on http://localhost:${PORT}`);
 })
 
-const db mysql.createConnection({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Azkhiyah903#',
@@ -37,7 +37,56 @@ app.get('/api/users', (req, res) => {
             return;
         }
         res.json(results); 
-    })
-})
+    });
+});
 
 
+app.post('/api/users', (req, res) => {
+    const {nama, nim, kelas} = req.body;
+
+    if (!nama || !nim || !kelas){
+        return req.status(400).json({message:'nama, nim, kelas wajin diisi'});
+    }
+
+db.query(
+    'INSERT INTO mahasiswa (nim, nama, kelas) VALUES (?,?,?)',
+    [nama, nim, kelas],
+    (err, results) => {
+        if(err){
+            console.error(err);
+            return res.status(500).json({message : 'Database Error'});
+        }
+
+        res.status(201).json({message : 'User Created successfully'});
+    }
+);
+});
+
+app.put('/api/users/:id', (req, res)=>{
+    const userId = req.params.id;
+    const {nama, nim, kelas} = req.body;
+    db.query(
+        'UPDATE mahasiswa SET nama = ?, nim = ?, kelas = ?, WHERE id = ? ',
+        [nama, nim,  kelas,UserId],
+        (err, results) => {
+            if (err) {
+                console.err(err);
+                return res.status(500).json({message: 'Database Error' });
+            }
+            res.json({message: 'User updated successfully!'});
+        }
+    );
+});
+
+app.delete('/api/users/:id', (req, res)=>{
+    const userId = req.params.id;
+    const {nama, nim, kelas} = req.body;
+    db.query(' DELETE FROM mahasiswa WHERE id = ?' [userID],(err, reults) =>{
+        if (err) {
+                console.error(err);
+                return res.status(500).json({message: 'Database Error' });
+            }
+             res.json({message: 'User updated successfully!'});
+        }
+    );
+});
